86dc2a06449c84914241cada6528d22c0cb9d073
iamdeepakvashisth <73371807+iamdeepakvashisth@users.noreply.github.com>
2021-04-15 18:10:32 -0400

    Update AddDigits.py

--- AddDigits.py
+++ AddDigits.py
@@ -1,4 +1,4 @@
- 258. Add Digits
+ #258. Add Digits
  #Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
  def addDigits(self, num: int) -> int:
         while num > 9:
