bd3835b3cd7908f66995b165c61e6489ce51cac1
iamdeepakvashisth <73371807+iamdeepakvashisth@users.noreply.github.com>
2021-04-23 23:49:45 -0400

    Multiply Strings

---
+++ MultiplyStrings.py
@@ -0,0 +1,10 @@
+def multiply(self, num1: str, num2: str) -> str:
+        return str(int(num1)*int(num2))
+        
+        
+"""
+
+Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
+
+Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.
+"""
