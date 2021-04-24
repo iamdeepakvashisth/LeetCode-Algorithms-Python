c55fe6027b134943ca203ed85905379559e89e73
iamdeepakvashisth <73371807+iamdeepakvashisth@users.noreply.github.com>
2021-04-23 23:35:00 -0400

    Add binary
    Given two binary strings a and b, return their sum as a binary string.
    
     
    Example 1:
    
    Input: a = "11", b = "1"
    Output: "100"

---
+++ AddBinary.py
@@ -0,0 +1,13 @@
+def addBinary(self, a: str, b: str) -> str:
+        return format((int(a,2)+int(b,2)),"b")
+        
+        
+"""
+Given two binary strings a and b, return their sum as a binary string.
+
+ 
+Example 1:
+
+Input: a = "11", b = "1"
+Output: "100"
+"""
