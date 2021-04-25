d1f769f71e937c00899dba61d3b99457eebf2f97
iamdeepakvashisth <73371807+iamdeepakvashisth@users.noreply.github.com>
2021-04-25 10:12:19 -0400

    Intersection of two arrays
    Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.

---
+++ IntersectionofTwoArrays.py
@@ -0,0 +1,7 @@
+def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return list(set(nums1).intersection(set(nums2)))
+        
+        
+"""
+Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+"""
