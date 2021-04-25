1bee08b80aa1b19103e852d048bf463085e01be5
iamdeepakvashisth <73371807+iamdeepakvashisth@users.noreply.github.com>
2021-04-24 20:57:28 -0400

    For faster execution, assigning it to a variable.

--- LengthOfLastWord.py
+++ LengthOfLastWord.py
@@ -1,5 +1,6 @@
 def lengthOfLastWord(self, s: str) -> int:
-        return len(s.strip().split(" ")[-1])
+        x=s.strip().split(" ")[-1]
+        return len(x)
         
 """
 Given a string s consists of some words separated by spaces, return the length of the last word in the string. If the last word does not exist, return 0.
