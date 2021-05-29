de9a66c0c51961099b83557936edec9a3c72e6d2
iamdeepakvashisth <73371807+iamdeepakvashisth@users.noreply.github.com>
2021-05-28 21:12:06 -0400

    Num Of Rectangles That Can Form The Largest Square
    You are given an array rectangles where rectangles[i] = [li, wi] represents the ith rectangle of length li and width wi.
    
    You can cut the ith rectangle to form a square with a side length of k if both k <= li and k <= wi. For example, if you have a rectangle [4,6], you can cut it to get a square with a side length of at most 4.
    
    Let maxLen be the side length of the largest square you can obtain from any of the given rectangles.
    
    Return the number of rectangles that can make a square with a side length of maxLen.

---
+++ LargestSquareFromRectangles.py
@@ -0,0 +1,15 @@
+'''
+You are given an array rectangles where rectangles[i] = [li, wi] represents the ith rectangle of length li and width wi.
+
+You can cut the ith rectangle to form a square with a side length of k if both k <= li and k <= wi. For example, if you have a rectangle [4,6], you can cut it to get a square with a side length of at most 4.
+
+Let maxLen be the side length of the largest square you can obtain from any of the given rectangles.
+
+Return the number of rectangles that can make a square with a side length of maxLen.
+'''
+
+def countGoodRectangles(self, rectangles: List[List[int]]) -> int:
+    a=[]
+    for i in rectangles:
+        a.append(min(i))
+    return a.count(max(a))
