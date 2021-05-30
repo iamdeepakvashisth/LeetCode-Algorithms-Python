05d1d8b2a227027102883bf6e2267240d1b1ca12
iamdeepakvashisth <73371807+iamdeepakvashisth@users.noreply.github.com>
2021-05-30 03:58:05 -0400

    First and Last Position of Element in Sorted Array
    Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
    
    If target is not found in the array, return [-1, -1].

---
+++ FirstAndLastIndexofElement.py
@@ -0,0 +1,17 @@
+'''
+Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+
+If target is not found in the array, return [-1, -1].
+'''
+def searchRange(self, nums: List[int], target: int) -> List[int]:
+        fisrtIndex,lastindex=-1,-1
+        if target not in nums:
+            return [fisrtIndex,lastindex]
+        else:
+            for i in range(len(nums)):
+                if nums[i]==target and fisrtIndex==-1:
+                    fisrtIndex=i
+                    lastindex=fisrtIndex
+                elif nums[i]==target and fisrtIndex>-1:
+                    lastindex=i
+        return [fisrtIndex,lastindex]
