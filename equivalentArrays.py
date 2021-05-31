b3abc8b3539b634c62984debbf1dae8f056dbb39
iamdeepakvashisth <73371807+iamdeepakvashisth@users.noreply.github.com>
2021-05-31 00:43:30 -0400

    Check If Two String Arrays are Equivalent
    Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
    
    A string is represented by an array if the array elements concatenated in order forms the string.

---
+++ equivalentArrays.py
@@ -0,0 +1,7 @@
+'''
+Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
+
+A string is represented by an array if the array elements concatenated in order forms the string.
+'''
+def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        return "".join(word1) == "".join(word2)
