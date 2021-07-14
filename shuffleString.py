66278161fc945cb62e17fa28d7050865e89689aa
iamdeepakvashisth <73371807+iamdeepakvashisth@users.noreply.github.com>
2021-07-14 00:21:31 -0400

    Shuffle String
    '''
    Given a string s and an integer array indices of the same length.
    
    The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
    
    Return the shuffled string.
    
    '''

---
+++ shuffleString.py
@@ -0,0 +1,14 @@
+'''
+Given a string s and an integer array indices of the same length.
+
+The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+
+Return the shuffled string.
+
+'''
+
+def restoreString(self, s: str, indices: List[int]) -> str:
+    newStr = ""
+    for i in range(len(indices)):
+        newStr += s[indices.index(i)]
+    return newStr
